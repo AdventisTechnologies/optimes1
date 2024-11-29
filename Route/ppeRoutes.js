e3412209ddcd587640ddbec0434066b43cbdd3a2
AdventisTechnologies <sales@adventis.tech>
2024-11-29 12:06:05 +0530

    Delete Route directory

--- Route/ppeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ppeController = require('../Controller/ppeController');
-
-router.post('/', ppeController.addPPEKit);
-router.get('/', ppeController.getAllPPEKits);
-
-
-module.exports = router;
