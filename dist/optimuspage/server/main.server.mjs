8f95bb0c51ec8770e926114b595e20f7b50b3183
AdventisTechnologies <sales@adventis.tech>
2024-11-29 11:22:58 +0530

    Delete dist/optimuspage directory

--- dist/optimuspage/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{d as r,e,g as t,h as o,j as p}from"./chunk-S2ZNBJQZ.mjs";import"./chunk-6YU7FWGA.mjs";import"./chunk-2KQF7ZJA.mjs";import"./chunk-H4UZCO6D.mjs";import a from"./angular-app-manifest.mjs";r(a);export{p as default,o as \u0275destroyAngularServerApp,e as \u0275extractRoutesAndCreateRouteTree,t as \u0275getOrCreateAngularServerApp};
